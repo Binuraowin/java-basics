2eb62cbe15a5d7b5a5d09537740f8d98c313c88b
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2023-02-27 01:07:30 +0530

    ref variables

--- Demo.java
+++ Demo.java
@@ -1,5 +1,20 @@
 public class Demo {
     public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        int calcRes = calculator.add(5,3);
+        System.out.println(calcRes);
         System.out.println("hello world");
+    }
+}
+
+/**
+ * Calculator
+ */
+class Calculator {
+
+    int a;
+    public int add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
     }
 }
